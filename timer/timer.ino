ae55ffa838602dc9496315ecff01145b49881f63
Edmund von der Burg <evdb@ecclestoad.co.uk>
2015-09-02 14:12:44 +0200

    Move existing constants to a header file

--- timer/timer.ino
+++ timer/timer.ino
@@ -1,29 +1,5 @@
-//Display brightness
-//
-//Each digit is on for a certain amount of microseconds Then it is off until we
-//have reached a total of DISPLAY_LOOP_TIME for the function call Let's assume
-//each digit is on for 1000us
-//
-//Each digit is on for 1ms, there are 4 digits, so the display is off for 16ms.
-//That's a ratio of 1ms to 16ms or 6.25% on time (PWM). Let's define a variable
-//called brightness that varies from:
-//
-//5000 blindingly bright (15.7mA current draw per digit)
-//2000 shockingly bright (11.4mA current draw per digit)
-//1000 pretty bright (5.9mA)
-//500 normal (3mA)
-//200 dim but readable (1.4mA)
-//50 dim but readable (0.56mA)
-//5 dim but readable (0.31mA)
-//1 dim but readable in dark (0.28mA)
-#define DISPLAY_BRIGHTNESS  500
-#define DISPLAY_LOOP_TIME 10
 
-#define DIGIT_ON  HIGH
-#define DIGIT_OFF  LOW
-
-#define SEGMENT_ON  LOW
-#define SEGMENT_OFF HIGH
+#include "constants.h"
 
 int digit1 = 11; //PWM Display pin 1
 int digit2 = 10; //PWM Display pin 2
@@ -38,6 +14,12 @@ int segE = A0; //Display pin 5
 int segF = 7; //Display pin 11
 int segG = 8; //Display pin 15
 int segColon = 12; // Display pin 12
+
+#define DIGIT_ON  HIGH
+#define DIGIT_OFF  LOW
+
+#define SEGMENT_ON  LOW
+#define SEGMENT_OFF HIGH
 
 void setup() {
   pinMode(segA, OUTPUT);
