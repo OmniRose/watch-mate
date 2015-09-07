b309472309f768268585433dc84a2eeb35cd1092
Edmund von der Burg <evdb@ecclestoad.co.uk>
2015-09-07 10:51:57 +0200

    Arrange the code for introducing the states

--- timer/timer.ino
+++ timer/timer.ino
@@ -9,6 +9,8 @@ Buttons buttons;
 unsigned long countdown_ends;
 unsigned long countdown_duration;
 
+#define STATE_RUNNING 1
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Starting up!");
@@ -23,21 +25,32 @@ void setup() {
 
 void loop() {
 
-  long time_remaining = countdown_ends - millis();
-
-  display.display_time( time_remaining / 1000 );
-
+  // Get the button press here and pass it to the state loops later.
   int button = buttons.get_button_press();
 
+  // Special case holding down the mode button as it has the same behaviour in
+  // all the states.
   if ( button == BUTTON_MODE_HELD_DOWN ) {
     enter_shutdown_state();
     return;
   }
 
-  if ( button == BUTTON_RESTART ) {
-    // reset the start time
-    countdown_ends = millis() + countdown_duration + 500;
+  int current_state = STATE_RUNNING;
+
+  switch (current_state) {
+    case STATE_RUNNING:
+      long time_remaining = countdown_ends - millis();
+
+      display.display_time( time_remaining / 1000 );
+
+
+      if ( button == BUTTON_RESTART ) {
+        // reset the start time
+        countdown_ends = millis() + countdown_duration + 500;
+      }
+      break;
   }
+
 
 }
 
